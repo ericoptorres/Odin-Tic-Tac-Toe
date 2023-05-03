6681b68d869c229c80fe9db4fcd8f5614dfebcb1
erico <erico_pt@hotmail.com>
2023-05-03 15:31:52 -0300

    basic game working

--- script.js
+++ script.js
@@ -1,44 +1,91 @@
 const gameBoard = (() => {
-    const array =  ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O']
-
+    const array =  ['', '', '', '', '', '', '', '', '']
     return {
        array
     }
 })()
 
-
 const player = (name) => {
-
+    
     return {name}
 }
 
+let activePlayer = player('player1').name
+
+const switchPlayer = () => {
+    if (activePlayer == 'player1'){
+        activePlayer = 'player2'
+    }
+    else {
+        activePlayer = 'player1'
+    }
+}
+
+const getMarkValue = (activePlayer) => {
+    let value = ''
+    if (activePlayer == 'player1'){
+        value = 'X'
+    }
+    else{
+        value = 'O'
+    }
+    return value
+}
+
+const available = (array, index) => {
+    if (array[index] == ''){
+        return true
+    }
+    else{
+        return false
+    } 
+}
 
 const display = () => {
-    const box = document.querySelectorAll('.box')
+    const boxes = document.querySelectorAll('.box')
 
-    let index = 0
-    box.forEach(elem => {
-        elem.textContent = gameBoard.array[index]
-        elem.dataset.position = index
-        index++
-
-        elem.addEventListener('click', function(){
-            changeArray(gameBoard.array, elem.dataset.position, 'X')
-            display()
-        })
+    boxes.forEach((box, index) => {
+        if (available(gameBoard.array, index)){
+            box.addEventListener('click', function(){
+                gameBoard.array[index] =  getMarkValue(activePlayer)
+                box.textContent = gameBoard.array[index]
+                checkGameOver(gameBoard.array)
+                switchPlayer()
+            }, {once: true})
+        }
     })
 }
 
 display()
 
-const changeArray = (array, index, value) => {
-    array[index] = value
+const checkGameOver = (array) => {
+    if (array[0] != '' && array[0] == array[1] && array[0] == array[2]){
+        alert('Game over!!!')
+    }
+    else if (array[3] != '' && array[3] == array[4] && array[3] == array[5]){
+        alert('Game over!!!')
+    }
+    else if (array[6] != '' && array[6] == array[7] && array[6] == array[8]){
+        alert('Game over!!!')
+    }
+    else if (array[0] != '' && array[0] == array[3] && array[0] == array[6]){
+        alert('Game over!!!')
+    }
+    else if (array[1] != '' && array[1] == array[4] && array[1] == array[7]){
+        alert('Game over!!!')
+    }
+    else if (array[2] != '' && array[2] == array[5] && array[2] == array[8]){
+        alert('Game over!!!')
+    }
+    else if (array[0] != '' && array[0] == array[4] && array[0] == array[8]){
+        alert('Game over!!!')
+    }
+    else if (array[2] != '' && array[2] == array[4] && array[2] == array[6]){
+        alert('Game over!!!')
+    }
 }
 
 
-const getValue = () => {
-    
-}
 
 
 
